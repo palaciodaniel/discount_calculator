0d0b893362f6e617efec914aea7ef2a14f51ccc5
Daniel Palacio <45110344+palaciodaniel@users.noreply.github.com>
2020-08-04 04:57:06 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name="discount_calculator",
+      version="1.5.0",
+      description="A function to calculate discounted prices",
+      author="Daniel Palacio")
